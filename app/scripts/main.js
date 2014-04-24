2757658b444c463ede094cd34071aa55f1df2362
Henrik Östman <henrik.ostman@svenskaspel.se>
2014-04-24 10:17:39 +0200

    Initial checkin - Yeoman with Backbone-generator.

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Blurry, $*/
+
+
+window.Blurry = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Blurry.init();
+});
