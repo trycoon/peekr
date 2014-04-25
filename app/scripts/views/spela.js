837fdcbd50184e2e9e5a5bee35c29320821d1d4e
Henrik Östman <henrik.ostman@svenskaspel.se>
2014-04-25 10:14:41 +0200

    Mer mask-fixar.

--- app/scripts/views/spela.js
+++ app/scripts/views/spela.js
@@ -15,6 +15,12 @@ Blurry.Views = Blurry.Views || {};
 
     className: '',
 
+    imagePlaceholder: undefined,
+    imageCanvas: undefined,
+    imageElement: undefined,
+    currentDifficulty: 3, // Index till holeSize, lägre = enklare.
+    holeSizes: [30, 20, 15, 10],
+
     events: {
       'click .js-validate': 'validateName'
     },
@@ -28,34 +34,66 @@ Blurry.Views = Blurry.Views || {};
 
     render: function() {
       this.$el.html(this.template(this.model.toJSON()));
+      this.imagePlaceholder = $('#image');
+      this.imageCanvas = this.imagePlaceholder[0].getContext('2d');
+
+      //this.imageCanvas.addEventListener('touchstart', startDraw);
+      this.imagePlaceholder.bind('touchmove', $.proxy(this.updateMaskPosition, this));
+      //this.imageCanvas.addEventListener('touchend', stopDraw);
+
+      //this.imageCanvas.addEventListener('mousedown', startDraw);
+      this.imagePlaceholder.bind('mousemove', $.proxy(this.updateMaskPosition, this));
+      //this.imageCanvas.addEventListener('mouseup', stopDraw);
 
       this.drawImage();
     },
 
+    updateMaskPosition: function(e) {
+      var self = this;
+
+      if (e.type.indexOf('touch') >= 0) {
+        var pos = e.touches.item(0);
+        self.drawMask(pos.clientX - pos.target.offsetLeft, pos.clientY - pos.target.offsetTop, self.currentHoleSize);
+      } else {
+        self.drawMask(e.offsetX, e.offsetY, self.holeSizes[self.currentDifficulty]);
+      }
+    },
+
     drawImage: function() {
-      var imageCanvas = document.getElementById('image').getContext('2d');
       this.currentImage = this.model.getImage();
-      var img = new Image();
+      var self = this;
 
-      img.onload = function() {
-        imageCanvas.save();
-
-        // Create a circle
-        /*imageCanvas.beginPath();
-         imageCanvas.arc(106, 77, 10, 0, Math.PI * 2, false);
-
-         // Clip to the current path
-         imageCanvas.clip();*/
-
-        imageCanvas.drawImage(img, 0, 0);
-
-        // Undo the clipping
-        imageCanvas.restore();
-        //maskCanvas.drawImage(img, 0, 0);
+      // Ladda ner bilden som skall visas.
+      this.imageElement = new Image();
+      this.imageElement.onload = function() {
+        // Maskera och visa bilden när den är nerladdad.
+        self.drawMask(110, 70, self.holeSizes[self.currentDifficulty]);
       };
 
-      img.src = this.currentImage.url;
+      this.imageElement.src = this.currentImage.url;
 
+    },
+
+    /***
+     * Ritar ut bilden med en mask(titthål).
+     * @param holeX {number} Titthålets X-position.
+     * @param holeY {number} Titthålets Y-position.
+     * @param holeRadius {number} Titthålets radie(storlek).
+     */
+    drawMask: function(holeX, holeY, holeRadius) {
+      this.imageCanvas.save();
+
+      // Create a circle
+      this.imageCanvas.beginPath();
+      this.imageCanvas.arc(holeX, holeY, holeRadius, 0, Math.PI * 2, false);
+
+      // Clip to the current path
+      this.imageCanvas.clip();
+
+      this.imageCanvas.drawImage(this.imageElement, 0, 0);
+
+      // Undo the clipping
+      this.imageCanvas.restore();
     },
 
     show: function() {
@@ -71,7 +109,6 @@ Blurry.Views = Blurry.Views || {};
       event.preventDefault();
       console.log('input ', this.model.validateName($('.js-input').val()));
     }
-
   });
 
 })();
